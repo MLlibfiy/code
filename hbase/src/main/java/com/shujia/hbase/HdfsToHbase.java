145eb80d7c052de2093369951d2ac207d71d6056
Teacher <mllib_fiy@163.com>
2019-03-11 11:12:55 +0800

    hbase预分区，mr自定义分区

--- hbase/src/main/java/com/shujia/hbase/HdfsToHbase.java
+++ hbase/src/main/java/com/shujia/hbase/HdfsToHbase.java
@@ -11,7 +11,9 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 import java.io.IOException;
 
@@ -22,33 +24,76 @@ import java.io.IOException;
 public class HdfsToHbase {
 
 
-    public static class ToHbaseMap extends Mapper<LongWritable, Text, Text, NullWritable> {
+    public static class ToHbaseMap extends Mapper<LongWritable, Text, Text, Text> {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            context.write(value, NullWritable.get());
+            context.write(new Text(value.toString().split("\t")[0]), value);
         }
     }
 
-    public static class HDFSToHbaseReducer extends TableReducer<Text, NullWritable, NullWritable> {
+    public static class HDFSToHbaseReducer extends TableReducer<Text, Text, NullWritable> {
         @Override
-        protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-            String[] student = key.toString().split("\t");
-            String id = student[0];
-            String name = student[1];
-            int age = Integer.parseInt(student[2]);
-            String gender = student[3];
-            String clazz = student[4];
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text value : values) {
+                String[] student = value.toString().split("\t");
+                String id = student[0];
+                String name = student[1];
+                int age = Integer.parseInt(student[2]);
+                String gender = student[3];
+                String clazz = student[4];
 
-            Put put = new Put(id.getBytes());
-            put.add("info".getBytes(), "name".getBytes(), name.getBytes());
-            put.add("info".getBytes(), "age".getBytes(), Bytes.toBytes(age));
-            put.add("info".getBytes(), "gender".getBytes(), gender.getBytes());
-            put.add("info".getBytes(), "clazz".getBytes(), clazz.getBytes());
+                Put put = new Put(id.getBytes());
+                put.add("info".getBytes(), "name".getBytes(), name.getBytes());
+                put.add("info".getBytes(), "age".getBytes(), Bytes.toBytes(age));
+                put.add("info".getBytes(), "gender".getBytes(), gender.getBytes());
+                put.add("info".getBytes(), "clazz".getBytes(), clazz.getBytes());
 
 
-            //数据写入hbase
-            context.write(NullWritable.get(), put);
+                //数据写入hbase
+                context.write(NullWritable.get(), put);
+            }
+        }
+    }
 
+    /**
+     * 自定义分区
+     */
+    public static class HDFSToHbasePartitioner extends Partitioner<Text, Text> {
+        @Override
+        public int getPartition(Text key, Text value, int numReduceTasks) {
+            String id = key.toString();
+            if (id.compareTo("15001000|") < 0) {
+                return 0;
+            }
+            if (id.compareTo("15001001|") < 0) {
+                return 1;
+            }
+            if (id.compareTo("15001002|") < 0) {
+                return 2;
+            }
+            if (id.compareTo("15001003|") < 0) {
+                return 3;
+            }
+            if (id.compareTo("15001004|") < 0) {
+                return 4;
+            }
+            if (id.compareTo("15001005|") < 0) {
+                return 5;
+            }
+            if (id.compareTo("15001006|") < 0) {
+                return 6;
+            }
+            if (id.compareTo("15001007|") < 0) {
+                return 7;
+            }
+            if (id.compareTo("15001008|") < 0) {
+                return 8;
+            }
+            if (id.compareTo("15001009|") < 0) {
+                return 9;
+            }
+
+            return 0;
 
         }
     }
@@ -58,16 +103,19 @@ public class HdfsToHbase {
         conf.set("hbase.zookeeper.quorum", "node1:2181,node2:2181,node3:2181");
         Job job = Job.getInstance(conf);
 
-        job.setNumReduceTasks(5);
+        job.setNumReduceTasks(10);
         job.setJarByClass(HdfsToHbase.class);
         job.setJobName("HdfsToHbase");
         job.setMapperClass(ToHbaseMap.class);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setPartitionerClass(HDFSToHbasePartitioner.class);//设置自定义分区器
+
+
         FileInputFormat.addInputPath(job, new Path("/data/out/student/"));
 
         //指定reduce
-        TableMapReduceUtil.initTableReducerJob("student", HDFSToHbaseReducer.class, job, null, null, null, null, false);
+        TableMapReduceUtil.initTableReducerJob("student1", HDFSToHbaseReducer.class, job, null, null, null, null, false);
 
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(Put.class);
---
+++ hbase/src/main/java/com/shujia/hbase/Test.java
@@ -0,0 +1,8 @@
+package com.shujia.hbase;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1500100196".compareTo("15001000|"));
+
+    }
+}
