7c3c5731a71741c1be658543ab696495ddf8d91e
Teacher <mllib_fiy@163.com>
2019-03-21 09:41:58 +0800

    scala 入门

---
+++ scala/src/main/java/com/shujia/scala/Test.scala
@@ -0,0 +1,17 @@
+package com.shujia.scala
+
+object Test {
+
+  /**
+    * def 关键字
+    * main  函数名
+    * args  方法的参数
+    *
+    * Unit  没有返回值 相当于void
+    *
+    */
+  def main(args: Array[String]): Unit = {
+    //底层调用了java的打印方法   PrintStream
+    println("hello world")
+  }
+}
